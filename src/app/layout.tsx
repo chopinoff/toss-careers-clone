39bb8919b473a8ab9b9a217f6701b09f4c8ca416
HY KIM <chopinoff@gmail.com>
2024-06-07 18:38:16 +0900

    style: Add Global Style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
 import type { Metadata } from 'next';
+import StyledComponentsRegistry from './registry';
 import './_styles/reset.css';
+import './_styles/global.css';
 
 export const metadata: Metadata = {
-    title: '토스채용',
-    description: '토스 채용 페이지 클론코딩입니다.',
-    icons: {
-        icon: '/images/favicon.svg',
-    },
+  title: '토스채용',
+  description: '토스 채용 페이지 클론코딩입니다.',
+  icons: {
+    icon: '/images/favicon.svg',
+  },
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+'use client';
+
+import { styled } from 'styled-components';
+
 export default function Home() {
-    return <main></main>;
+  return (
+    <main>
+      <Test>test</Test>
+    </main>
+  );
 }
+
+const Test = styled.div`
+  background-color: var(--test);
+`;
