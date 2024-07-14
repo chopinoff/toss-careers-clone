0eea9131580cab87d4a1280756186fd037252e99
HY KIM <chopinoff@gmail.com>
2024-07-14 15:15:30 +0900

    Fix: Refresh Scroll Position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,15 @@ import { styled } from 'styled-components';
 import ScrollSection from './_components/Home/ScrollSection';
 import ContentSection from './_components/Home/ContentSection';
 import FooterSection from './_components/Home/FooterSection';
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    window.onbeforeunload = function pushRefresh() {
+      window.scrollTo(0, 0);
+    };
+  }, []);
+
   return (
     <Main>
       <ScrollSection />
